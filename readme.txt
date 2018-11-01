b750f2969720fd2c71cbbc9a9c7b58ae242b2b0f
Popov Evgeniy <tuzik87@inbox.ru>
2018-11-01 18:48:39 +0600

    First stable version

---
+++ readme.txt
@@ -0,0 +1,42 @@
+                                SIMPLE UPX WIZARD
+
+                                  Version 0.7
+
+                         This software made by Popov Evgeniy Alekseyevich
+
+System requirement
+
+Windows 2000 or higher for 32 bit system
+Windows Vista or higher for 64 bit system
+
+Program description
+
+This program is a graphic shell for UPX.
+
+License
+
+This program distributed under GNU GENERAL PUBLIC LICENSE.
+
+Prepare for work
+
+You can download UPX here: https://upx.github.io/
+Put executable file of UPX to SIMPLE UPX WIZARD directory before first start.
+
+Contact
+
+You can send me a letter to tuzik87@inbox.ru
+
+About program modification
+
+Source code is included in source.zip archive.
+It can be compiled under Lazarus 1.8.4 or higher.
+
+Change log
+
+0.1 — Initial version
+0.2 - Small changes
+0.3 - New options was added
+0.4 - New options was added
+0.5 - Small changes
+0.6 - User interface was improved
+0.7 - 64 bit support was added
---
+++ simpleupxwizardcode.pas
@@ -0,0 +1,224 @@
+unit simpleupxwizardcode;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  ExtCtrls, StdCtrls, LazUTF8;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    CheckBox4: TCheckBox;
+    CheckBox5: TCheckBox;
+    Label1: TLabel;
+    LabeledEdit1: TLabeledEdit;
+    LabeledEdit2: TLabeledEdit;
+    OpenDialog1: TOpenDialog;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TrackBar1: TTrackBar;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+procedure window_setup();
+procedure dialog_setup();
+function convert_file_name(source:string): string;
+function execute_program(executable:string;argument:string):Integer;
+procedure interface_setup();
+procedure language_setup();
+procedure setup();
+function get_option():string;
+procedure compress_file(target:string);
+procedure decompress_file(target:string);
+
+var Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+procedure window_setup();
+begin
+ Application.Title:='Simple upx wizard';
+ Form1.Caption:='Simple upx wizard 0.7';
+ Form1.BorderStyle:=bsDialog;
+ Form1.Font.Name:=Screen.MenuFont.Name;
+ Form1.Font.Size:=14;
+end;
+
+procedure dialog_setup();
+begin
+Form1.OpenDialog1.InitialDir:='';
+Form1.OpenDialog1.FileName:='*.exe';
+Form1.OpenDialog1.DefaultExt:='*.exe';
+Form1.OpenDialog1.Filter:='Executable files|*.exe';
+end;
+
+function convert_file_name(source:string): string;
+var target:string;
+begin
+target:=source;
+if Pos(' ',source)>0 then
+begin
+target:='"'+source+'"';
+end;
+convert_file_name:=target;
+end;
+
+function execute_program(executable:string;argument:string):Integer;
+var code:Integer;
+begin
+try
+code:=ExecuteProcess(UTF8ToWinCP(executable),UTF8ToWinCP(argument),[]);
+except
+On EOSError do code:=-1;
+end;
+execute_program:=code;
+end;
+
+procedure interface_setup();
+begin
+Form1.Button2.Enabled:=False;
+Form1.Button4.Enabled:=False;
+Form1.CheckBox1.Checked:=False;
+Form1.CheckBox2.Checked:=False;
+Form1.CheckBox3.Checked:=False;
+Form1.CheckBox4.Checked:=True;
+Form1.CheckBox5.Checked:=False;
+Form1.LabeledEdit1.Enabled:=False;
+Form1.LabeledEdit2.Enabled:=False;
+Form1.LabeledEdit1.LabelPosition:=lpLeft;
+Form1.LabeledEdit2.LabelPosition:=lpLeft;
+Form1.LabeledEdit1.Text:='';
+Form1.LabeledEdit2.Text:='';
+Form1.TrackBar1.Orientation:=trHorizontal;
+Form1.TrackBar1.TickStyle:=tsAuto;
+Form1.TrackBar1.Min:=0;
+Form1.TrackBar1.Max:=9;
+Form1.TrackBar1.Position:=9;
+Form1.PageControl1.ActivePageIndex:=0;
+end;
+
+procedure language_setup();
+begin
+Form1.LabeledEdit1.EditLabel.Caption:='Target file';
+Form1.LabeledEdit2.EditLabel.Caption:='Target file';
+Form1.Button1.Caption:='Open';
+Form1.Button2.Caption:='Compress';
+Form1.Button3.Caption:='Open';
+Form1.Button4.Caption:='Decompress';
+Form1.OpenDialog1.Title:='Open an executable file';
+Form1.PageControl1.Pages[0].Caption:='Compression';
+Form1.PageControl1.Pages[1].Caption:='Decompression';
+Form1.CheckBox1.Caption:='Dont compress export section';
+Form1.CheckBox2.Caption:='Dont compress resources';
+Form1.CheckBox3.Caption:='Dont compress icons';
+Form1.CheckBox4.Caption:='Dont strip relocations';
+Form1.CheckBox5.Caption:='Create backup';
+Form1.Label1.Caption:='Compress ratio';
+end;
+
+procedure setup();
+begin
+window_setup();
+dialog_setup();
+interface_setup();
+language_setup();
+end;
+
+function get_option():string;
+var option:string;
+var ratio:array[0..9] of string=('-1 ','-2 ','-3 ','-4 ','-5 ','-6 ','-7 ','-8 ','-9 ','--best ');
+begin
+option:=ratio[Form1.TrackBar1.Position];
+if Form1.CheckBox1.Checked=True then option:=option+'--compress-export=0 ';
+if Form1.CheckBox2.Checked=True then option:=option+'--compress-resources=0 ';
+if Form1.CheckBox3.Checked=True then option:=option+'--compress-icons=0 ';
+if Form1.CheckBox4.Checked=True then option:=option+'--strip-relocs=0 ';
+if Form1.CheckBox5.Checked=True then option:=option+'--backup ';
+get_option:=option;
+end;
+
+procedure compress_file(target:string);
+var backend,option:string;
+begin
+backend:=ExtractFilePath(Application.ExeName)+'upx';
+option:=get_option()+convert_file_name(target);
+if execute_program(backend,option)=-1 then
+begin
+ShowMessage('Cant compress target file');
+end;
+
+end;
+
+procedure decompress_file(target:string);
+var backend,option:string;
+begin
+backend:=ExtractFilePath(Application.ExeName)+'upx';
+option:='-d '+convert_file_name(target);
+if execute_program(backend,option)=-1 then
+begin
+ShowMessage('Cant decompress target file');
+end;
+
+end;
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+setup();
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+if Form1.OpenDialog1.Execute()=True then
+begin
+Form1.Button2.Enabled:=True;
+Form1.LabeledEdit1.Text:=Form1.OpenDialog1.FileName;
+end;
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+compress_file(Form1.LabeledEdit1.Text);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+if Form1.OpenDialog1.Execute()=True then
+begin
+Form1.Button4.Enabled:=True;
+Form1.LabeledEdit2.Text:=Form1.OpenDialog1.FileName;
+end;
+
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+decompress_file(Form1.LabeledEdit2.Text);
+end;
+
+end.
